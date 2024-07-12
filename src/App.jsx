4f4b414f9d495d5ea29ac882060bfae7c4735934
junhyeon0218 <junhyeon0218@daum.net>
2024-07-12 16:26:41 +0900

    📦️ Chore: axios, dotenv 설치 및 파일구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import './styles/global.css';
+import Landing from './pages/landing';
+import Signup from './pages/Signup';
+import Signin from './pages/signin';
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+        </>
+    );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count good {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route path='landing' element={<Landing />} />
+            </Route>
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signin' element={<Signin />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
