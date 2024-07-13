d7e580fef694c2204871094d5304ea7fcfd90bff
junhyeon0218 <junhyeon0218@daum.net>
2024-07-13 19:12:43 +0900

    ✨ Feat: header 제작

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const [user, setUser] = useState(false);
+    const navigate = useNavigate();
+
     return (
-        <>
-            <h1 className='text-20 text-gray-50'>GitPotato</h1>
-        </>
+        <div className='flex h-100 w-full items-center px-10 shadow-custom-dark'>
+            <div className='mx-auto flex h-48 w-[calc(100%-500px)] justify-between wide:w-1400'>
+                <div>
+                    <h1 className='h-48 w-150 text-32 font-bold'>GitPotato</h1>
+                </div>
+                {user ? (
+                    <div>프로필</div>
+                ) : (
+                    <div className='flex'>
+                        <button
+                            className='h-44 w-90 rounded-4 bg-primary font-bold text-white duration-200 hover:scale-105'
+                            onClick={() => navigate('/signin')}
+                        >
+                            Sign In
+                        </button>
+                        <button
+                            className='ml-25 h-44 w-90 rounded-4 border-1 border-primary font-bold text-primary duration-200 hover:scale-105'
+                            onClick={() => navigate('/signup')}
+                        >
+                            Sign Up
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 };
 
