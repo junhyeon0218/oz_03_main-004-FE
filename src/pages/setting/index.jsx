86e687d45e718d2aaad960904e762c0942a295a6
Watnu03 <watnu03@gmail.com>
2024-07-18 13:01:25 +0900

    🎨 Design: Nickname 변경 버튼 수정

--- src/pages/setting/index.jsx
+++ src/pages/setting/index.jsx
@@ -25,63 +25,63 @@ const Setting = () => {
     const [error, setError] = useState({ nickname: '' });
     const [isFormValid, setIsFormValid] = useState(false);
     // Github, Baekjoon 연동 (연동되지않았다=false)
-    const [isGithubConnected, setIsGithubConnected] = useState(false);
     const [isBaekjoonConnected, setIsBaekjoonConnected] = useState(false);
+    const [isEditing, setIsEditing] = useState(false); // 닉네임 편집 상태
 
     // Nickname에 error 또는 빈 input 인지 확인
     const validateForm = () => {
         return Object.values(error).every((err) => !err) && nickname.trim() !== '';
     };
 
-    useEffect(
-        () => {
-            // 사용자 id별 Setting Page 로드
-            const fetchUser = async () => {
-                try {
-                    const response = await axios.get(`setting:${id}`);
-                    setNickname(response.data.nickname);
-                } catch (error) {
-                    console.error('Failed to fetch user from server', error);
-                }
-            };
+    useEffect(() => {
+        // 사용자 id별 Setting Page 로드
+        const fetchUser = async () => {
+            try {
+                const response = await axios.get(`setting:${id}`);
+                setNickname(response.data.nickname);
+            } catch (error) {
+                console.error('Failed to fetch user from server', error);
+            }
+        };
 
-            // 사용자 Nickname 가져오기
-            const getNickname = async () => {
-                try {
-                    const serverNickname = await fetchNicknameFromServer();
-                    setNickname(serverNickname);
-                } catch (err) {
-                    console.error('Failed to fetch nickname from server', err);
-                    setNickname('serverNickname');
-                }
-            };
+        // 사용자 Nickname 가져오기
+        const getNickname = async () => {
+            try {
+                const serverNickname = await fetchNicknameFromServer();
+                setNickname(serverNickname);
+            } catch (err) {
+                console.error('Failed to fetch nickname from server', err);
+                setNickname('serverNickname');
+            }
+        };
 
-            // Github, Baekjoon 연동 상태 가져오기
-            const fetchConnections = async () => {
-                try {
-                    // const response = await axios.get(`/api/account-connections`);
-                    // setIsGithubConnected(response.data.github);
-                    // setIsBaekjoonConnected(response.data.baekjoon);
+        // Github, Baekjoon 연동 상태 가져오기
+        const fetchConnections = async () => {
+            try {
+                // const response = await axios.get(`/api/account-connections`);
+                // setIsGithubConnected(response.data.github);
+                // setIsBaekjoonConnected(response.data.baekjoon);
 
-                    // 서버통신 전 버튼작동 확인 용 임시 코드 (나중에 삭제하기)
-                    const response = { data: { github: false, baekjoon: false } };
-                    setIsGithubConnected(response.data.github);
-                    setIsBaekjoonConnected(response.data.baekjoon);
-                } catch (error) {
-                    console.error('Failed to fetch account connections', error);
-                    setIsGithubConnected(false);
-                    setIsBaekjoonConnected(false);
-                }
-            };
+                // 서버통신 전 버튼작동 확인 용 임시 코드 (나중에 삭제하기)
+                const response = { data: { github: false, baekjoon: false } };
+                setIsGithubConnected(response.data.github);
+                setIsBaekjoonConnected(response.data.baekjoon);
+            } catch (error) {
+                console.error('Failed to fetch account connections', error);
+                setIsGithubConnected(false);
+                setIsBaekjoonConnected(false);
+            }
+        };
 
-            fetchUser();
-            getNickname();
-            fetchConnections();
-        },
-        // [id],
-        // (나중에 삭제하기)
-        []
-    );
+        fetchUser();
+        getNickname();
+        fetchConnections();
+    }, [id]);
+
+    // Nickname 입력창 클릭 시 편집 상태로 전환
+    const handleClickNicknameInput = () => {
+        setIsEditing(true);
+    };
 
     // Nickname Rename
     const handleNicknameChange = async () => {
@@ -92,6 +92,8 @@ const Setting = () => {
                 // await axios.put('/api/nickname', { nickname });
                 // setNickname(nickname);
                 // setError({ ...error, nickname: '' });
+
+                setIsEditing(false);
                 alert('Nickname updated successfully');
             } catch (error) {
                 console.error('Failed to change nickname', error);
@@ -138,46 +140,52 @@ const Setting = () => {
         <div className='m-ful flex h-[100vh] w-full flex-col items-center'>
             <div className='relative h-250 w-[calc(100%-500px)] min-w-627 pt-50'>
                 <h1 className='h-auto min-w-0 border-b border-gray-300 text-32'>General</h1>
-                <div className='mt-15 flex items-center'>
-                    <FormInput
-                        id='userNickname'
-                        label='My Nickname'
-                        type='text'
-                        placeholder='Please Enter Your Nickname'
-                        error={error.nickname}
-                        setError={(errorMsg) => setError({ ...error, nickname: errorMsg })}
-                        onChange={(e) => setNickname(e.target.value)}
-                        value={nickname}
-                        isFormValid={isFormValid}
-                        setIsFormValid={setIsFormValid}
-                    />
-                    {/* 반응형추가하기 */}
-                    <button
-                        onClick={handleNicknameChange}
-                        className='absolute left-520 top-154 ml-4 h-44 w-101 rounded bg-primary text-white duration-200 hover:scale-105'
-                    >
-                        Rename
-                    </button>
+                <div className='mt-15'>
+                    <div className='flex items-start'>
+                        <div className='h-150'>
+                            {isEditing ? (
+                                <FormInput
+                                    id='userNickname'
+                                    label='My Nickname'
+                                    type='text'
+                                    placeholder='Please Enter Your Nickname'
+                                    error={error.nickname}
+                                    setError={(errorMsg) => setError({ ...error, nickname: errorMsg })}
+                                    onChange={(e) => setNickname(e.target.value)}
+                                    value={nickname}
+                                    isFormValid={isFormValid}
+                                    setIsFormValid={setIsFormValid}
+                                    className='w-520'
+                                />
+                            ) : (
+                                <div className='my-8 inline-flex h-auto w-auto flex-col items-start justify-start gap-2'>
+                                    <label htmlFor={id} className='mb-3 text-16 text-black'>
+                                        My Nickname
+                                    </label>
+                                    <span
+                                        onClick={handleClickNicknameInput}
+                                        className='inline-flex h-50 cursor-pointer items-center justify-start gap-2.5 rounded-4 border bg-white px-16 py-15'
+                                    >
+                                        {nickname}
+                                    </span>
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            onClick={handleNicknameChange}
+                            className={`${
+                                isEditing ? 'bg-primary' : 'bg-gray-db'
+                            } ml-4 mt-40 h-44 w-101 rounded text-white duration-200`}
+                            disabled={!isEditing}
+                        >
+                            Rename
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className='relative h-250 w-[calc(100%-500px)] min-w-627 pt-50'>
                 <h1 className='h-auto min-w-0 border-b border-gray-300 text-32'>Connect</h1>
                 <div className='mt-15 flex w-520 flex-col'>
-                    <div className='ml-3 flex h-55 w-300 items-center justify-between'>
-                        <span className='flex items-center'>
-                            <img src='../../../public/potato.svg' className='h-42 w-42 rounded-full bg-blue' />
-                            <p className='ml-5 text-20'>Github</p>
-                        </span>
-                        <button
-                            className={`left-535 top-142 ml-4 h-44 w-108 rounded text-16 text-white duration-200 hover:scale-105 ${
-                                isGithubConnected ? 'bg-gray-db' : 'bg-primary'
-                            }`}
-                            onClick={() => !isGithubConnected && handleConnect('Github')}
-                            disabled={isGithubConnected}
-                        >
-                            {isGithubConnected ? 'Connected' : 'Connect'}
-                        </button>
-                    </div>
                     <div className='ml-3 flex h-55 w-300 items-center justify-between'>
                         <span className='flex items-center'>
                             <img src='../../../public/potato.svg' className='h-42 w-42 rounded-full bg-blue' />
