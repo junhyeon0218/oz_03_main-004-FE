0ba77f9298115868087496a7d2c1b92089ebef00
junhyeon0218 <junhyeon0218@daum.net>
2024-07-26 21:36:06 +0900

    ✨ Feat: alert 기능 생성

---
+++ src/components/common/alert/index.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import alertSuccess from '../../../assets/images/alertSuccess.svg';
+import alertWarn from '../../../assets/images/alertWarn.svg';
+
+const Alert = ({ text, type, onClose }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onClose();
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [onClose]);
+
+    return (
+        <div
+            className={`absolute bottom-20 right-20 flex h-45 w-auto items-center justify-evenly rounded-8 px-20 py-12 ${type === 'success' ? 'bg-[#1BCC73]' : 'bg-[#F54025]'}`}
+        >
+            <img src={type === 'success' ? alertSuccess : alertWarn} alt={type} className='w-20 h-20' />
+            <p className='text-white text-16'>{text}</p>
+        </div>
+    );
+};
+
+export default Alert;
