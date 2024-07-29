6bb5e45c59783213fbd75d5321fd8d1e346e6308
Watnu03 <watnu03@gmail.com>
2024-07-29 11:45:54 +0900

    📦️ Chore: 모달 index 폴더로export 경로 합침

---
+++ src/components/modal/index.js
@@ -0,0 +1,4 @@
+export { default as BaekjoonConnectModal } from './BaekjoonConnectModal';
+export { default as ChangePotatoModal } from './ChangePotatoModal';
+export { default as PotatoInfoModal } from './PotatoInfoModal';
+export { default as UserUpdateModal } from './UserUpdateModal';
