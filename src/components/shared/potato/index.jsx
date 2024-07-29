0fca1b6094e53c6c2899561dd12c54f6f8add952
akwjr963 <akwjr963@daum.net>
2024-07-29 16:07:13 +0900

    ✨ Feat: Potato 컴포넌트 생성

--- src/components/shared/potato/index.jsx
+++ src/components/shared/potato/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
 import Loading from '../../common/loading';
 import Alert from '../../common/alert';
+import React, { useEffect, useState } from 'react';
+import coinImage from '../../../../public/potato/coin.svg';
 
 const Potato = ({ selectedImage }) => {
+    const [coinCount, setCoinCount] = useState(0);
+
+    useEffect(() => {
+        // 서버에서 코인 개수를 받아오는 함수
+        const fetchCoins = async () => {
+            try {
+                const response = await fetch('https://my-api-endpoint.com/coins'); // API 엔드포인트로 변경예정
+                const data = await response.json();
+                setCoinCount(data.coins);
+            } catch (error) {
+                console.error('Error fetching coin count:', error);
+            }
+        };
+
+        fetchCoins();
+    }, []);
+
     return (
-        <div className='w-full h-full overflow-hidden'>
-            {selectedImage ? ( // 선택된 이미지가 있으면 이미지를 표시하고, 없으면 메시지를 표시합니다.
-                <img src={`/collection/${selectedImage}`} alt='Selected Potato' />
+        <div className='flex flex-col items-center w-auto h-auto'>
+            <div className='flex justify-center w-full'>
+                <img src={coinImage} className='mr-5 mb-30 h-25 w-55 text-14' alt='Coin' /> : {coinCount}
+            </div>
+            {selectedImage ? (
+                <img src={selectedImage} alt='Selected Potato' className='scale-150 h-100 w-120' />
             ) : (
                 <p>No potato selected.</p>
             )}
