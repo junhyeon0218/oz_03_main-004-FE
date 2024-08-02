176b906486516e4aa6356864da67d4e29ee10db3
Watnu03 <watnu03@gmail.com>
2024-08-02 11:13:18 +0900

    🎨 Design: 레벨업이미지추가 및 로딩컴포넌트 css수정

--- src/components/common/loading/index.jsx
+++ src/components/common/loading/index.jsx
@@ -3,9 +3,11 @@ import potato from '../../../assets/images/jump.gif';
 
 const Loading = () => {
     return (
-        <div className='absolute flex items-center justify-center gap-10'>
-            <img src={potato} className='w-40' alt='' />
-            <p className='mt-10 text-20 font-light tracking-widest text-gray-98'>Loading...</p>
+        <div className='fixed inset-0 z-50 flex items-center justify-center bg-white'>
+            <div className='absolute flex items-center justify-center gap-10'>
+                <img src={potato} className='w-40' alt='' />
+                <p className='mt-10 text-20 font-light tracking-widest text-gray-98'>Loading...</p>
+            </div>
         </div>
     );
 };
