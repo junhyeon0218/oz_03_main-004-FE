231dd56feb2142dec93fa616b1b1db92353c0bd0
Watnu03 <watnu03@gmail.com>
2024-08-02 11:12:19 +0900

    📝 Docs: 경로마지막 / 추가

--- src/apis/api/stack.js
+++ src/apis/api/stack.js
@@ -2,22 +2,22 @@ import { authInstance } from '../utils/instance';
 
 export const stackAPI = {
     getAllStacks: async () => {
-        const { data } = await authInstance.get('/stacks/all');
+        const { data } = await authInstance.get('/stacks/all/');
         return data;
     },
 
     getUserStacks: async () => {
-        const { data } = await authInstance.get('/stacks');
+        const { data } = await authInstance.get('/stacks/');
         return data;
     },
 
     createUserStack: async (stackId) => {
-        const { data } = await authInstance.post('/stacks/create', { stack_id: stackId });
+        const { data } = await authInstance.post(`/stacks/create/`, { stack_id: stackId });
         return data;
     },
 
     deleteUserStack: async (stackId) => {
-        const { data } = await authInstance.delete(`/stacks/delete`, { data: { stack_id: stackId } });
+        const { data } = await authInstance.delete(`/stacks/delete/${stackId}/`);
         return data;
     },
 };
