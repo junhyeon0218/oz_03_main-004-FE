6fc4731d4fc30d332470e8a05072b6818ed0fd5b
akwjr963 <akwjr963@daum.net>
2024-08-03 23:35:47 +0900

    ♻️ Refactor: 감자 이미지 일정 width이상일때 사이즈 조정

--- src/components/shared/potato/index.jsx
+++ src/components/shared/potato/index.jsx
@@ -63,7 +63,7 @@ const Potato = () => {
                     <img
                         src={`/src/assets/images/${selectedPotato.name}.png`}
                         alt='Selected Potato'
-                        className='h-118 w-85'
+                        className='h-118 w-85 hwide:h-120 hwide:w-150'
                     />
                 </div>
             ) : (
