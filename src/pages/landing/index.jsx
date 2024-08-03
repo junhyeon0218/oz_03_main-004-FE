95892f8438b24c620c64715f3e0a41900d3fab50
junhyeon0218 <junhyeon0218@daum.net>
2024-08-03 23:53:14 +0900

    🐛 자동로그인 기능 수정

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -6,24 +6,24 @@ const Section1 = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='flex items-center justify-center w-full px-10 overflow-hidden h-512'>
-            <div className='flex flex-col items-center justify-center h-full mx-auto w-1400'>
-                <div className='flex items-center justify-between w-full tablet:flex-col'>
+        <div className='flex h-512 w-full items-center justify-center overflow-hidden px-10'>
+            <div className='mx-auto flex h-full w-1400 flex-col items-center justify-center'>
+                <div className='flex w-full items-center justify-between tablet:flex-col'>
                     <div className='z-30'>
                         <img src='/images/section1.png' alt='' />
                     </div>
 
                     <div className='z-20 tablet:flex tablet:flex-col tablet:items-center tablet:justify-center'>
-                        <h1 className='font-bold text-40 text-strong tablet:text-32'>
+                        <h1 className='text-40 font-bold text-strong tablet:text-32'>
                             Grow Your Potato
                             <span className='text-28'> with </span>
                             GitHub Commits
                         </h1>
-                        <p className='font-semibold mt-21 text-20'>
+                        <p className='mt-21 text-20 font-semibold'>
                             The fun experience of growing potatoes <br /> through coding activity
                         </p>
                         <button
-                            className='font-bold text-white duration-200 mt-26 h-44 w-90 rounded-4 bg-primary hover:scale-105'
+                            className='mt-26 h-44 w-90 rounded-4 bg-primary font-bold text-white duration-200 hover:scale-105'
                             onClick={() => navigate('/signin')}
                         >
                             Get Start
@@ -46,19 +46,19 @@ const Section1 = () => {
 
 const Section2 = () => {
     return (
-        <div className='flex items-center justify-center w-full px-10 overflow-hidden h-512 bg-gray-bg'>
-            <div className='flex items-center justify-between h-full mx-auto w-1400 tablet:flex-col-reverse'>
+        <div className='flex h-512 w-full items-center justify-center overflow-hidden bg-gray-bg px-10'>
+            <div className='mx-auto flex h-full w-1400 items-center justify-between tablet:flex-col-reverse'>
                 <div className='flex flex-col items-end tablet:items-center'>
-                    <h1 className='font-bold text-40 text-strong tablet:text-32'>
+                    <h1 className='text-40 font-bold text-strong tablet:text-32'>
                         Plan Your Day with To-Do and Calendar!
                     </h1>
-                    <p className='font-semibold mt-28 text-20 tablet:mt-15'>
+                    <p className='mt-28 text-20 font-semibold tablet:mt-15'>
                         Stay Organized and On Track with Daily To-Do Lists
                     </p>
-                    <p className='font-semibold mt-28 text-20 tablet:mt-15'>
+                    <p className='mt-28 text-20 font-semibold tablet:mt-15'>
                         Visualize Your Progress with an Intuitive Calendar
                     </p>
-                    <p className='font-semibold mt-28 text-20 tablet:my-15'>
+                    <p className='mt-28 text-20 font-semibold tablet:my-15'>
                         Seamlessly Sync Your Tasks and Events for Maximum Efficiency
                     </p>
                 </div>
@@ -73,19 +73,19 @@ const Section2 = () => {
 
 const Section3 = () => {
     return (
-        <div className='flex items-center justify-center w-full px-10 overflow-hidden h-512'>
-            <div className='flex items-center justify-between h-full mx-auto w-1400 tablet:flex-col'>
+        <div className='flex h-512 w-full items-center justify-center overflow-hidden px-10'>
+            <div className='mx-auto flex h-full w-1400 items-center justify-between tablet:flex-col'>
                 <div className='tablet:w-450'>
                     <img src='/images/section3.png' alt='' />
                 </div>
                 <div className='flex flex-col items-center'>
-                    <h1 className='font-bold text-40 text-strong tablet:text-32'>Verified User Reviews</h1>
-                    <p className='w-2/3 ml-0 mr-auto font-semibold mt-28 text-20 tablet:mt-15 tablet:w-4/5'>
+                    <h1 className='text-40 font-bold text-strong tablet:text-32'>Verified User Reviews</h1>
+                    <p className='ml-0 mr-auto mt-28 w-2/3 text-20 font-semibold tablet:mt-15 tablet:w-4/5'>
                         This app has made coding so much more fun! Watching my potato grow with each commit is
                         incredibly motivating. <span className='text-16 text-gray-98'>- John Doe</span>
                     </p>
 
-                    <p className='w-2/3 ml-auto mr-0 font-semibold mt-28 text-20 tablet:mt-15 tablet:w-4/5'>
+                    <p className='ml-auto mr-0 mt-28 w-2/3 text-20 font-semibold tablet:mt-15 tablet:w-4/5'>
                         The to-do and calendar features are fantastic. They help me stay organized and on track with my
                         projects. <span className='text-16 text-gray-98'>- Jane</span>
                     </p>
@@ -96,6 +96,14 @@ const Section3 = () => {
 };
 
 const Landing = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            navigate('/home');
+        }
+    }, [navigate]);
     return (
         <>
             <Section1 />
